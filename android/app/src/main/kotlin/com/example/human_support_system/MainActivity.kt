1b24c31997f02b041a716b231cdfde86f0006679
Robertino <tinoroki@gmail.com>
2020-09-22 10:01:36 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/human_support_system/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.human_support_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/signup.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Signup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: new SignupPage(),
+    );
+  }
+}
+
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+
+class _SignupPageState extends State<SignupPage> {
+
+  DateTime selectedDate = DateTime.now().toLocal();
+
+  _selectDate(BuildContext context) async {
+    bool _decideWhichDayToEnable(DateTime day) {
+      if ((day.isAfter(DateTime.now().subtract(Duration(days: 1))) &&
+          day.isBefore(DateTime.now().add(Duration(days: 31))))) {
+        return true;
+      }
+      return false;
+    }
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate, // Refer step 1
+      firstDate: DateTime(1000),
+      lastDate: DateTime(2050),
+      selectableDayPredicate: _decideWhichDayToEnable,
+      helpText: 'Date of birth', // Can be used as title
+      cancelText: 'Cancel',
+      confirmText: 'Done',
+    );
+    if (picked != null)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(15.0, 60.0, 0.0, 0.0),
+            alignment: Alignment.center,
+            child: Text(
+              'HSS Sign Up',
+              style: TextStyle(
+                fontSize: 55.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 35.0,left: 20.0,right: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blueAccent,)
+                      )
+                  ),
+                ),
+                SizedBox(height: 10.0,),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blueAccent,)
+                      )
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 10.0,),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Confirm your password',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blueAccent,)
+                      )
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 10.0,),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'First Name',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blueAccent,)
+                      )
+                  ),
+                ),
+                SizedBox(height: 10.0,),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Last Name',
+                      labelStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blueAccent,)
+                      )
+                  ),
+                ),
+                SizedBox(height: 10.0,),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                        Text(
+                          'Date of Birth',
+                          style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                        ),
+                        Container(
+                          height: 40.0,
+                          width: 95.0,
+                          alignment: Alignment.center,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20.0),
+                            shadowColor: Colors.blue,
+                            color: Colors.blueAccent,
+                            elevation: 7.0,
+                            child: GestureDetector(
+                              onTap: () => _selectDate(context), // Refer step 3
+                              child: Text(
+                                "${selectedDate.toLocal()}".split(' ')[0],
+                                style:
+                                TextStyle(color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                ),
+                SizedBox(height: 20.0,),
+                Container(
+                  height: 40.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.blue,
+                    color: Colors.blueAccent,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: (){},
+                      child: Center(
+                        child: Text(
+                          'Register',
+                          style: TextStyle(
+                            color:Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10.0,),
+                Container(
+                  height: 40.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.grey[600],
+                    color: Colors.white,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: (){
+
+                      },
+                      child: Center(
+                        child: Text(
+                          'Go Back',
+                          style: TextStyle(
+                            color:Colors.blueAccent,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
